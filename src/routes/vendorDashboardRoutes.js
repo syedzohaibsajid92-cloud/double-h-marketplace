73e9ffb897918ba27ee5888c07600d21bcbcec75
Zohaib <syedzohaibsajid92@gmail.com>
2026-08-08 18:06:29 +0500

    fix: correct vendorDashboardController casing in routes

--- src/routes/vendorDashboardRoutes.js
+++ src/routes/vendorDashboardRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getVendorDashboard } = require("../controllers/vendordashboardController");
+const { getVendorDashboard } = require("../controllers/vendorDashboardController");
 
 // Destructure verifyToken (or verifyVendor) from authMiddleware
 const { verifyToken } = require("../middleware/authMiddleware");
